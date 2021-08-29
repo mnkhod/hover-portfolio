9fbb4f3b94ab7bbb9d7e64a3bff086093514c073
mnkhod <mnkhod.dev@gmail.com>
2021-08-29 08:27:53 +0800

    Added Mobile Responsiveness To Home Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,2 @@
 particlesJS.load('particle-container', 'assets/js/particle-config.json', function() {});
+particlesJS.load('particle-mobile-container', 'assets/js/particle-mobile-config.json', function() {});
